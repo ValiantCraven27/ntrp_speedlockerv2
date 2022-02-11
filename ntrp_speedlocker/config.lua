1ae681a7c5f767a7a6ccf8867fe0152340aaa493
Brwnlee <77315942+ValiantCraven27@users.noreply.github.com>
2022-02-10 20:50:39 -0500

    Update config.lua

--- ntrp_speedlocker/config.lua
+++ ntrp_speedlocker/config.lua
@@ -4,7 +4,7 @@ Config.MenuTitle = 'NTRP Suspension v1.1'
 
 Config.UseSpeedLock = true
 Config.SpeedKey = 68    --LB or Right Mouse Click | https://docs.fivem.net/docs/game-references/controls/
-Config.RideKey = 76 -- Double Tap RB quickly
+Config.RideKey = 76 -- Double Tap RB or Spacebar quickly
 Config.noWeapons = true   -- No driver drive bys
 Config.fuelWarning = true  -- fuel level warning
 Config.fuelPercent = 20 -- what level to warn you about fuel
